da4d805e96d205d86f3fb7ba76c7252da5766766
Supun Sithmal <supunsithmal@gmail.com>
2018-09-10 23:59:18 +0530

    Inital Commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Hello World</h1>
+        <button className="btn btn-primary m-2">Login</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
